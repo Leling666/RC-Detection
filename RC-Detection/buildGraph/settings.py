83cff0be0990a5c2baefa787951b5f4e37e7c1b5
ZhangJQ+7 <111476787+Leling666@users.noreply.github.com>
2024-10-07 19:25:16 +0800

    Delete RC-Detection directory

--- RC-Detection/buildGraph/settings.py
+++
@@ -1,5 +0,0 @@
-REPOS_DIR = ""
-SZZ_FOLDER = "../ICSE2022ReplicationPackage/icse2021-szz-replication-package"
-REPOS_DIR = ""
-ast_map_path = "../ICSE2022ReplicationPackage/ASTMapEval_jar"
-DATA_PATH = "../trainData"
